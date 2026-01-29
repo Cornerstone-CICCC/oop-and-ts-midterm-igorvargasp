ea0fd457a66e3aab5376c0b5d95192f221efbc69
igorvargasp <igorvargasp@hotmail.com>
2026-01-28 23:02:07 -0300

    feat: add navbar with centered nav, underline active state, and hero section

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import { Component } from "../common/Component";
+import { Component } from "../common/Component.js";
+import { Header } from "./Header.js";
+import { Hero } from "./Hero.js";
 
 export class App extends Component {
   render() {
-    
+    const app = document.createElement("div");
+    app.className = "app";
+
+    const header = new Header({ cartContext: this.props.cartContext });
+    header.mount(app);
+
+    const hero = new Hero();
+    hero.mount(app);
+
+    return app;
   }
 }
